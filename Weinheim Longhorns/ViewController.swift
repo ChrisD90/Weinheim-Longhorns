989490bf027008a6b47de7e7c2a13f9de28b3287
Christoph Doeringer <christoph.doeringer@gmail.com>
2020-03-20 17:45:27 +0100

    Initial Commit

---
+++ Weinheim Longhorns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weinheim Longhorns
+//
+//  Created by Chris on 20.03.20.
+//  Copyright © 2020 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
